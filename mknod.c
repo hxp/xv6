e64771b422267c273b928784921848de54ea5121
Joel Heikkila <jjheik@gmail.com>
2012-02-04 05:04:16 -0500

    Initial commit based on xv6-rev6 with a couple simple commands added:
    mknod
    mv
    test

---
+++ mknod.c
@@ -0,0 +1,19 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+  if(argc != 4){
+    printf(2, "Usage: mknod devicefile minor major\n");
+    exit();
+  }
+
+ if(mknod(argv[1], atoi(argv[2]), atoi(argv[3])) < 0) {
+   printf(2, "unable to create device file %s\n",argv[1]);
+   exit();
+ }
+
+  exit();
+}
---
+++ mv.c
@@ -0,0 +1,16 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+  if(argc != 3){
+    printf(2, "Usage: mv source dest\n");
+    exit();
+  }
+  if(link(argv[1], argv[2]) < 0)
+    printf(2, "can't move %s: failed\n", argv[1]);
+  unlink(argv[1]);
+  exit();
+}
